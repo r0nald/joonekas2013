b1fd0529670460834b48036438f2de51c244abf2
Ronald T <ronaldt@kth.se>
2013-11-03 21:39:09 +0200

    Created two projects

--- joonekas.c
+++ joonekas.c
@@ -3,14 +3,15 @@
 #include "Comm.h"
 #include "pwm_out.h"
 
+#include "stm32f4xx_gpio.h"
+
 static uint32_t time = 0;
 static InputMsg inMsg;
 
 void Joonekas_SysTick(void)
 {
 	OutputMsg outMsg;
-	
-	
+		
 	if(Comm_NewMsg())
 	{
 		inMsg = Comm_LastMsg();
@@ -21,5 +22,8 @@ void Joonekas_SysTick(void)
 	outMsg.time = time;
 	outMsg.lineSensors = 0;
 	outMsg.fval = inMsg.leftPwm;
+	outMsg.lineSensors = GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_11)<<4 
+													| GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_10);
+	
 	Comm_SendOutMsg(&outMsg);
 }
