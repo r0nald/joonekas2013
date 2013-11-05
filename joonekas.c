7df988547f0392d2c7e9d28882bb392e1d5d8ecb
Ronald T <ronaldt@kth.se>
2013-11-05 21:10:07 +0200

    Refactored UART DMA Start to separate file.

--- joonekas.c
+++ joonekas.c
@@ -3,8 +3,7 @@
 #include "Comm.h"
 #include "pwm_out.h"
 #include "adc.h"
-
-#include "stm32f4xx.h"
+#include "dma_uart.h"
 
 static uint32_t time = 0;
 static InputMsg inMsg;
@@ -28,12 +27,5 @@ void Joonekas_SysTick(void)
 	Comm_SendOutMsg(&outMsg);
 	
 	Comm_OutMsgToStr(&outMsg, (char*)Comm_TxBuffer, &txPacketLen);
-	
-	/* Clear DMA Transfer Complete Flags */
-  DMA_ClearFlag(DMA2_Stream6,DMA_FLAG_TCIF6);
-	DMA_ClearFlag(DMA2_Stream6,DMA_FLAG_HTIF6);
-  /* Clear USART Transfer Complete Flags */
-  USART_ClearFlag(USART6,USART_FLAG_TC);
-	/* DMA transfer should start now */
-	DMA_Cmd(DMA2_Stream6, ENABLE);
+	DmaUart_StartTx();
 }
