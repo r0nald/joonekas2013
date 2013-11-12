cc8ef690398051a887133a847aadd61e3890e926
Ronald <ronaldt@kth.se>
2013-11-12 23:32:24 +0200

    plotting PID, configuring PID

--- py-comm/logRobot.py
+++ py-comm/logRobot.py
@@ -7,6 +7,7 @@ import curses
 from HexStrDataParser import HexStrDataParser, DataToHexStr
 import datetime
 import subprocess
+import curses.textpad as textpad
 
 StartByte = chr(2)
 EndByte = chr(10)
@@ -25,8 +26,10 @@ curses.cbreak()
 stdscr.keypad(1)
 stdscr.nodelay(1)
 
-currentDataWin = curses.newwin(2, 100, 5, 0)
+currentDataWin = curses.newwin(3, 80, 5, 0)
 currentDataWin.nodelay(1)
+
+PIDconfWin = curses.newwin(5, 80, 10, 0)
 
 '''
 uint32_t 	time;					# 0	
@@ -39,19 +42,20 @@ float			pidK, pidI, pidD, pidU;		# 5 6 7 8
 float			pwmLeft, pwmRight;		# 9 10
 
 float			battVoltage;			# 11
+uint8_t			finishLineDetected;		# 12
 '''
 
-parser = HexStrDataParser('IIII' + 'f' + 'ffff' + 'fff')
+parser = HexStrDataParser('IIII' + 'f' + 'ffff' + 'fff' + 'b')
 
 '''
-float 		leftPwm, rightPwm;	# 0 1
-float		pidK, pidI, pidF;	# 2 3 4
-uint32_t 	cmdType;		# 5
+float 		leftPwm, rightPwm;		# 0 1
+float		basePwm, pidK, pidI, pidD;	# 2 3 4 5
+uint32_t 	cmdType;			# 6
 enum CommandFromPC { Stop = 0, Run = 1, SetPid = 2, SetPwm  = 3 };
 '''
 
-packetBuilder = DataToHexStr('ff' + 'fff' + 'I')
-packetStruct = [0.2, 0.2, 0.0, 0.0, 0.0, 0]
+packetBuilder = DataToHexStr('ff' + 'ffff' + 'I')
+packetStruct = [0.2, 0.2, 0.0, 0.0, 0.0, 0.0, 0]
 
 def sendPacketStruct(packetStruct):
 	global sercom
@@ -61,17 +65,23 @@ def sendPacketStruct(packetStruct):
 
 def sendStop():
 	global packetStruct
-	packetStruct[5] = 0
+	packetStruct[6] = 0
 	sendPacketStruct(packetStruct)
 
 def sendStart():
 	global packetStruct
-	packetStruct[5] = 1
+	packetStruct[6] = 1
 	sendPacketStruct(packetStruct)
 
 def sendPwm():
 	global packetStruct
-	packetStruct[5] = 3
+	packetStruct[6] = 3
+	sendPacketStruct(packetStruct)
+
+def sendControllerConf(conf):
+	global packetStruct
+	packetStruct[2:6] = conf
+	packetStruct[6] = 2
 	sendPacketStruct(packetStruct)
 
 firstLine = False
@@ -79,6 +89,9 @@ running = False
 logFile = None
 fname = None
 firstTime = None
+conf = None
+lastFinishTime = None
+prevFinishLine = 0
 
 while True:
 	sercom.readline()
@@ -99,6 +112,8 @@ while True:
 	except Exception as e:
 		dataOk = False
 
+	currentDataWin.clear()
+
 	if dataOk:
 		timeTick = data[0]
 		lineSensors = data[1]
@@ -107,6 +122,10 @@ while True:
 		leftPwm = data[9]
 		rightPwm = data[10]
 		pidU = data[8]
+		pidP = data[5]
+		pidI = data[6]
+		pidD = data[7]
+		finishLine = data[8]
 
 		if not firstTime:
 			firstTime = timeTick
@@ -116,15 +135,28 @@ while True:
 		debugLine = '{0:0b}'.format(lineSensors).zfill(10) + ' ' +  '{0:0b}'.format(usedSensors).zfill(10) + ' ' + str(pidFeedback) + ' ' + str(pidU)
 
 		if logFile:
-			logLine = '{} {} {} {} {}'.format(time, pidFeedback, pidU, leftPwm, rightPwm)
+			logLine = '{} {} {} {} {} {} {} {}'.format(time, pidFeedback, pidU, leftPwm, rightPwm, pidP, pidI, pidD)
 			logFile.write(logLine +'\n')
+
+		currentDataWin.addstr(0, 0, debugLine)
+		currentDataWin.addstr(1, 0, str(leftPwm) + ' ' + str(rightPwm))
+
+		if finishLine == 1:
+			print 'FINISH'
+			break
+
+		if finishLine == 1 and prevFinishLine == 0:
+			if lastFinishTime:
+				now = datetime.datetime.now()
+				timer = (now - lastFinishTime).total_seconds()
+				currentDataWin.addstr(2, 0, 'Lap time : ' + str(timer))
+			lastFinishTime = now
+			currentDataWin.addstr(2, 0, 'Finish')
+		prevFinishLine = finishLine
 
 	else:
 		currentDataWin.addstr(0, 0, 'No comm')
 	
-	currentDataWin.clear()
-	currentDataWin.addstr(0, 0, debugLine)
-	currentDataWin.addstr(1, 0, str(leftPwm) + ' ' + str(rightPwm))
 	currentDataWin.refresh()
 
 	c = stdscr.getch()
@@ -141,6 +173,7 @@ while True:
 			subprocess.Popen('./plot_control1 ' + fname, shell=True)
 		else:
 			fname = datetime.datetime.now().strftime('run%d%H%M%S.log')
+			lastFinishTime = None
 			logFile = open(fname, 'w')
 			sendStart()
 			running = True
@@ -150,6 +183,14 @@ while True:
 		sendPwm()
 		stdscr.addstr(0, 0, 'PWM  ')
 		running = True
+
+	elif c == ord('c'):
+		tp = textpad.Textbox(PIDconfWin)
+		paramStr = tp.edit()
+		conf = [float(fstr) for fstr in paramStr.rsplit()]
+		if len(conf) != 4:
+			PIDconfWin.addstr(0, 0, 'Invalid params')
+		sendControllerConf(conf)
 
 
 curses.nocbreak(); stdscr.keypad(0); curses.echo()
